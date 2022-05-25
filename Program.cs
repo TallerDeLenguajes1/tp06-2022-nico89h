323ecff36b3911e247047a78058319329141c536
Nicolas Leal <nicoleal14h@gmail.com>
2022-05-25 17:32:00 -0300

    otro commit

--- Program.cs
+++ Program.cs
@@ -3,36 +3,47 @@ Console.WriteLine("Hello, World!");
 
 Calculadora jose = new Calculadora();
 bool xd=true;
+int nico=0;
 while (xd)
 {
     Console.WriteLine("Introduci el primer valor a dar");
-    double x=(double)Console.ReadLine():
+    jose.X=Double.Parse(Console.ReadLine());
     Console.WriteLine("Introduci el 2do valor a dar");
-    double x=Console.ReadLine():
+    jose.Y=Double.Parse(Console.ReadLine());
+    Console.WriteLine("La suma es 10+20: " + jose.suma());
+    Console.WriteLine("La multiplicacion de 10 y 20: " + jose.multiplicacion());
+    Console.WriteLine("La division  de 20 y 10: " + jose.division());
+    Console.WriteLine("La resta  de 20 y 10: " + jose.resta());
+    Console.WriteLine("Queres seguir con la calculadora pibe");
+    nico=Int32.Parse(Console.ReadLine());
+    if (nico!=0)
+    {
+        xd=false;
+    }
 }
-Console.WriteLine("La suma es 10+20: " + jose.suma(10,20));
-Console.WriteLine("La multiplicacion de 10 y 20: " + jose.multiplicacion(10,20));
-Console.WriteLine("La division  de 20 y 10: " + jose.multiplicacion(10,20));
-Console.WriteLine("La resta  de 20 y 10: " + jose.resta(10,20));
-
-
 
 public class Calculadora
 {
-    double x;
-    double y;
-
-    public double suma(double x, double y){
+    private double x;
+    private double y;
+    public double X{
+        get=>x;
+        set=>x=value;
+    } 
+    public double Y{
+        get=>y;
+        set=>y=value;
+    }
+    public double suma(){
         return x+y;
     }
-    public double multiplicacion(double x, double y){
+    public double multiplicacion(){
         return x*y;
     }
-    public double division(double x, double y){
+    public double division(){
         return x/y;
     }
-    public double resta(double x, double y){
+    public double resta(){
         return x-y;
     }
-
 }
